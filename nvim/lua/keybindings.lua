0a588e486b3b258b1ebfc08b6fc1f759dfca20b0
Kenny Issak <kennyissak@gmail.com>
2022-09-05 23:43:44 +0800

    add split window setting and terminal function

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -19,11 +19,29 @@ map("n", "<A-h>", "<C-w>h", opt)
 map("n", "<A-j>", "<C-w>j", opt)
 map("n", "<A-k>", "<C-w>k", opt)
 map("n", "<A-l>", "<C-w>l", opt)
+-- 左右比例控制
+map("n", "s.", ":vertical resize -20<CR>", opt)
+map("n", "s,", ":vertical resize +20<CR>", opt)
+-- 上下比例
+map("n", "sj", ":resize +10<CR>", opt)
+map("n", "sk", ":resize -10<CR>", opt)
+-- 等比例
+map("n", "s=", "<C-w>=", opt)
+
+-- Terminal相关
+map("n", "<leader>t", ":sp | terminal<CR>", opt)
+map("n", "<leader>vt", ":vsp | terminal<CR>", opt)
+map("t", "<Esc>", "<C-\\><C-n>", opt)
+map("t", "<A-h>", [[ <C-\><C-N><C-w>h ]], opt)
+map("t", "<A-j>", [[ <C-\><C-N><C-w>j ]], opt)
+map("t", "<A-k>", [[ <C-\><C-N><C-w>k ]], opt)
+map("t", "<A-l>", [[ <C-\><C-N><C-w>l ]], opt)
+
 
 -- 退出
-map("n", "<C-q>", ":q<CR>", opt)
+map("n", "q", ":q<CR>", opt)
 -- 保存退出
-map("n", "<C-s>", ":wq<CR>", opt)
+map("n", "<C-s>", ":w<CR>", opt)
 
 -- bufferline
 -- 左右Tab切换
